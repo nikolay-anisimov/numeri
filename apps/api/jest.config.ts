0ae36f2c0fd6324a21f3c2f1dce58bfe03169a20
Nikolai Anisimov <nick.anisimov@hotmail.com>
2025-08-24 16:12:46 +0200

    test(api): map TS path aliases in Jest via moduleNameMapper

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -3,8 +3,13 @@ import type { Config } from 'jest'
 const config: Config = {
   testEnvironment: 'node',
   transform: { '^.+\\.(t|j)s$': ['ts-jest', { tsconfig: 'tsconfig.json' }] },
-  testRegex: '.*\\.spec\\.ts$'
+  testRegex: '.*\\.spec\\.ts$',
+  moduleNameMapper: {
+    '^@packages/utils$': '<rootDir>/../../packages/utils/src/index.ts',
+    '^@packages/ui$': '<rootDir>/../../packages/ui/src/index.ts',
+    '^@utils/(.*)$': '<rootDir>/../../packages/utils/src/$1',
+    '^@ui/(.*)$': '<rootDir>/../../packages/ui/src/$1'
+  }
 }
 
 export default config
-
